fefe091e90ee055ab9f68130f6d49a8a28a62be3
micnem00 <michael.nemni@gmail.com>
2024-10-21 23:34:13 +0300

    redirect if user tp dashboard

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,8 +1,13 @@
 import { HydrateClient } from "@/trpc/server";
 import LandingPage from "@/components/LandingPage";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
-
+  const user = await currentUser();
+  if (user) {
+    redirect(`${process.env.NEXT_PUBLIC_APP_URL}/dashboard`);
+  }
   const session = null;
   return (
     <HydrateClient>
