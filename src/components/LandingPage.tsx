2f09cd97c269de81d6039b18265a2cfd33be1621
micnem00 <michael.nemni@gmail.com>
2024-10-21 23:28:21 +0300

    fix sign in button styling

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -130,7 +130,12 @@ export default function LandingPage({ session }: { session: Session | null }) {
                   {session && <span>Logged in as {session.user?.name}</span>}
                 </p>
                 <SignedOut>
-                  <SignInButton />
+                  <SignInButton>
+                    <button
+                      className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20">
+                      Sign in
+                    </button>
+                  </SignInButton>
                 </SignedOut>
                 <SignedIn>
                   <button
