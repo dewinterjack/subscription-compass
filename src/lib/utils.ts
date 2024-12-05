430614a5c4eee7d6f17cf2b4fcf6c4470bc76cf3
micnem00 <michael.nemni@gmail.com>
2024-12-05 17:04:13 +0200

    remove plaid ref

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,20 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
-import type { Database } from "@/server/db";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
-}
-
-export async function getPlaidAccessToken(userId: string, db: Database) {
-  const plaidItem = await db.plaidItem.findFirst({
-    where: {
-      userId: userId,
-    },
-  });
-  const accessToken = plaidItem?.accessToken;
-  if (!accessToken) {
-    throw new Error("No access token found");
-  }
-  return { accessToken, itemId: plaidItem?.id };
 }
